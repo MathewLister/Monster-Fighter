88d268e24834a517cb6d963e54ccc6c071ca6802
MathewLister <listermathew0@gmail.com>
2020-09-06 15:02:45 -0700

    added fighting

--- app.js
+++ app.js
@@ -10,6 +10,40 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function() {
+            let max = 10;
+            let min = 3;
+            let damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.monsterHealth -= damage;
+            
+            //Check if monster dead
+            if (this.monsterHealth <= 0) {
+                alert('You won!');
+                this.gameIsRunning = false;
+                return;
+            }
+
+            max = 12;
+            min = 5;
+            damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.playerHealth -= damage;
+
+            //Check if player dead
+            if (this.playerHealth <= 0) {
+                alert('You lost!');
+                this.gameIsRunning = false;
+                return;
+            }
+        },
+        specialAttack: function() {
+
+        },
+        heal: function() {
+
+        },
+        giveUp: function() {
+
         }
     }
 })
